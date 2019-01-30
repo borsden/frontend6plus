5a31f2e0086a2115a81b116e775196b0eaf06b31
Denis Borsh <borsden@gmail.com>
2019-01-31 02:06:39 +0300

    Updates

---
+++ precache-manifest.4f99319d03044b9801714b8e01077f9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "292fb8bb62f7137403b9",
+    "url": "/frontend6plus/static/js/runtime~main.292fb8bb.js"
+  },
+  {
+    "revision": "40e47a172daecb20f483",
+    "url": "/frontend6plus/static/js/main.40e47a17.chunk.js"
+  },
+  {
+    "revision": "8aa6a38d343088e76747",
+    "url": "/frontend6plus/static/js/1.8aa6a38d.chunk.js"
+  },
+  {
+    "revision": "40e47a172daecb20f483",
+    "url": "/frontend6plus/static/css/main.9cfff018.chunk.css"
+  },
+  {
+    "revision": "fb9cd93c3622f3de2901cbd610c9ddf5",
+    "url": "/frontend6plus/index.html"
+  }
+];
